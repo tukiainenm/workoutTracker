70a45fb3fe59d214ffc1493d04501d00b77ed516
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-04-21 17:40:00 +0300

    Tarvittavat templatet lisätty, bootstrap.min.css lisätty, Controlleria muokattu

--- src/main/java/com/example/workoutTracker/controllers/workoutController.java
+++ src/main/java/com/example/workoutTracker/controllers/workoutController.java
@@ -1,4 +1,26 @@
 package com.example.workoutTracker.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
 public class workoutController {
+
+    @RequestMapping(value = "/home")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/tracker")
+    public String track() {
+        return "tracker";
+    }
+
+    @RequestMapping(value = "/calculator")
+    public String calculate() {
+        return "calculator";
+    }
 }
+
